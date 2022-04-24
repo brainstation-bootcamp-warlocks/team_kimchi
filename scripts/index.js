06ac1c0bde715f6f59466d326948d43558bdaf7e
JumaMac <mjustinmacaraig@gmail.com>
2022-04-24 16:47:31 -0400

    added footer details through javascript

--- scripts/index.js
+++ scripts/index.js
@@ -55,7 +55,7 @@ class Footer {
         return `
         <div class="margin">
         <div class="footer-container">
-        <h2 class="footer__header">Get in Touch</h2>
+        <h2 class="footer__header">Follow Us</h2>
         <div class="footer__icon-container">
             <a class='primary-footer__link' href="https://instagram.com">
                 <img
@@ -76,6 +76,32 @@ class Footer {
                 />
             </a>
         </div>
+
+        <h2>Become Bartender<h2>
+
+        <div class="primary-footer__info-container">
+			<div>
+				<ul class="primary-footer__list">
+					<li class="primary-footer__list-item primary-footer__list-item--bold">Park Sejin</li>
+					<li class="primary-footer__list-item primary-footer__list-item--bold">Manhattan Management</li>
+				</ul>
+				<ul class="primary-footer__list">
+					<li class="primary-footer__list-item">24 Kennedy Penthouse,</li>
+					<li class="primary-footer__list-item">Brooklyn, NY 53831, USA</li>
+				</ul>
+				<a href="mailto:myfakeemail@email.com" class="primary-footer__email">myreallyfakeEmail@fake.com</a>
+			</div>
+			<div>
+				<ul class="primary-footer__list">
+					<li class="primary-footer__list-item primary-footer__list-item--bold">Mark Macaraig</li>
+					<li class="primary-footer__list-item primary-footer__list-item--bold">Limitless Drinking Groups</li>
+				</ul>
+				<ul class="primary-footer__list ">
+					<li class="primary-footer__list-item">59 Bremner Rd</li>
+					<li class="primary-footer__list-item">New York, </li>
+				</ul>
+				<a href="mailto:pearl.gregg@limitlessag.com" class="primary-footer__email">pearl.gregg@limitlessag.com</a>
+			</div>
         </div>
         </div>
         `;
