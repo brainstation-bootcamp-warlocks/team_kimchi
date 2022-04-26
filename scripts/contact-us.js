e95caaa7843e044855c312b29440205dc807f419
sjpark538 <phi5388@gmail.com>
2022-04-25 21:04:22 -0400

    scss update

--- scripts/contact-us.js
+++ scripts/contact-us.js
@@ -3,20 +3,17 @@ let contactContainer = document.getElementById("contact__container");
 let form = document.createElement("form");
 form.setAttribute("action", "");
 form.setAttribute("method", "post");
+form.classList.add("contact__form")
 contactContainer.appendChild(form);
 
 let heading = document.createElement('h2')
 heading.innerHTML = "Contact Us"
+heading.classList.add("contact__heading")
 contactContainer.appendChild(heading);
 
-let line = document.createElement('hr');
-contactContainer.appendChild(line);
-
-let linebreak1 = document.createElement('br');
-contactContainer.appendChild(linebreak1);
-
 let nameLabel = document.createElement('label');
-nameLabel.innerHTML = "Name :";
+nameLabel.innerHTML = "Name";
+nameLabel.classList.add("contact__name")
 contactContainer.appendChild(nameLabel);
 
 let inputEl = document.createElement ('input');
@@ -24,11 +21,9 @@ inputEl.setAttribute("type", "text");
 inputEl.setAttribute("name", "names");
 contactContainer.appendChild(inputEl);
 
-let linebreak2 = document.createElement('br');
-contactContainer.appendChild(linebreak2);
-
 let emailLabel = document.createElement('label');
-emailLabel.innerHTML = "Email :";
+emailLabel.innerHTML = "Email";
+emailLabel.classList.add("contact__email")
 contactContainer.appendChild(emailLabel);
 
 let emailEl = document.createElement('input')
@@ -36,22 +31,21 @@ emailEl.setAttribute("type", "text");
 emailEl.setAttribute("name", "emails");
 contactContainer.appendChild(emailEl);
 
-let linebreak3 = document.createElement('br');
-contactContainer.appendChild(linebreak3);
 
 let messageLabel = document.createElement('label'); 
-messageLabel.innerHTML = "Message : ";
+messageLabel.innerHTML = "Message";
+messageLabel.classList.add("contact__message")
 contactContainer.appendChild(messageLabel);
 
 let textareaEl = document.createElement('textarea');
 textareaEl.setAttribute("name", "messages");
+textareaEl.setAttribute("cols", "10");
+textareaEl.setAttribute("rows", "5");
 contactContainer.appendChild(textareaEl);
-
-let linebreak4 = document.createElement('br');
-contactContainer.appendChild(linebreak4);
 
 let  submitEl = document.createElement('input'); 
 submitEl.setAttribute("type", "submit");
 submitEl.setAttribute("name", "dsubmit");
-submitEl.setAttribute("value", "Submit");
+submitEl.setAttribute("value", "SUBMIT");
+submitEl.classList.add("contact__submit")
 contactContainer.appendChild(submitEl);
